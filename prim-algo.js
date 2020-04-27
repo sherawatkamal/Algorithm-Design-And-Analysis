a6b656022c6c179e1daaa2f99b0a54a1fa4607ad
kamal sherawat <55201291+sherawatkamal@users.noreply.github.com>
2020-04-27 14:26:44 +0530

    Update prim-algo.js

--- prim-algo.js
+++ prim-algo.js
@@ -4,24 +4,16 @@ primsMST() {
       return MST;
    }
 
-
-
    let s = this.nodes[0];
-
-
-
+   
    let edgeQueue = new PriorityQueue(this.nodes.length * this.nodes.length);
    let explored = new Set();
    explored.add(s);
    MST.addNode(s);
 
-
-
    this.edges[s].forEach(edge => {
       edgeQueue.enqueue([s, edge.node], edge.weight);
    });
-
-
 
    let currentMinEdge = edgeQueue.dequeue();
    while (!edgeQueue.isEmpty()) {
@@ -31,7 +23,6 @@ primsMST() {
       }
       let nextNode = currentMinEdge.data[1];
 
-
       if (!explored.has(nextNode)) {
          MST.addNode(nextNode);
          MST.addEdge(currentMinEdge.data[0], nextNode, currentMinEdge.priority);
@@ -40,8 +31,6 @@ primsMST() {
             edgeQueue.enqueue([nextNode, edge.node], edge.weight);
          });
 
-
-         // Mark this node as explored explored.add(nextNode);
          s = nextNode;
       }
    }
